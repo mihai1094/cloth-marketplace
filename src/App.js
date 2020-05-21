deff301c86daa2f657723d0b78f9df4ca5246286
mihaicat10 <mihai.ionescu1094@gmail.com>
2020-05-21 22:31:43 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+    return (
+        <div>
+            <Route exact={true} path='/' component={HomePage}/>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+// Read this - REACT ROUTER :)
+// {/*  if exact is not used, /cats will show Homepage too */}
+// {/*  If we have something like this with id, do not use exact path on it */}
+// {/*<Route exact={true} path='/topics' component={TopicComponent}*/}
+// {/*<Route path='/topics/:topicId' component={TopicComponent}*/}
+//
+// {/*For links ( navbar etc ) */}
+// {/*<Link to="/hats"></Link> */}
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -3,8 +3,8 @@ import MenuItem from "../menu-item/menu-item.component";
 import "./directory.styles.scss";
 
 class Directory extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       sections: [
@@ -12,38 +12,44 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
+          linkUrl: 'hats'
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
+          linkUrl: 'jackets'
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
+          linkUrl: 'sneakers'
         },
         {
           title: "womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
           id: 4,
           size: "large",
+          linkUrl: 'womens'
         },
         {
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           id: 5,
           size: "large",
+          linkUrl: 'mens'
         },
       ],
     };
   }
   render() {
+
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => {
+        {this.state.sections.map(({ id, ...otherSectionProps}) => {
           return (
-            <MenuItem key={id} title={title} size={size} imageUrl={imageUrl} />
+            <MenuItem  key={id} {...otherSectionProps} />
           );
         })}
       </div>
