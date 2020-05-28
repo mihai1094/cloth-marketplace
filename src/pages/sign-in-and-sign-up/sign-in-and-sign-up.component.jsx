5b66c4e9cda251627ff46cd2a266f5b2ec868090
mihaicat10 <mihai.ionescu1094@gmail.com>
2020-05-28 13:27:43 +0300

    sign in

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './sign-in-and-sign-out.scss'
+import './sign-in-and-sign-up.scss'
+import SignIn from "../../components/sign-in/sign-in.component";
 
 const SignInAndSignUpPage = () => {
     return (
         <div className='sign-in-and-sign-up'>
-            SignInAndSignUpPage
+            <SignIn/>
         </div>
     )
 }
