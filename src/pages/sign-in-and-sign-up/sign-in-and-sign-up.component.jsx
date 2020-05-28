2460090c35751746f3927ee476d8935db0430481
mihaicat10 <mihai.ionescu1094@gmail.com>
2020-05-28 13:27:38 +0300

    sign in

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './sign-in-and-sign-out.scss'
+
+const SignInAndSignUpPage = () => {
+    return (
+        <div className='sign-in-and-sign-up'>
+            SignInAndSignUpPage
+        </div>
+    )
+}
+export default SignInAndSignUpPage;
